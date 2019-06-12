c5b5cf4b750b1fcbbe19a46b474a68b502eabd4e
Alb The Wizard <alberto98fx@gmail.com>
2019-06-12 23:31:45 +0200

    Update auth.py

--- auth-function-mesh/auth.py
+++ auth-function-mesh/auth.py
@@ -9,10 +9,15 @@ This modules demonstrates a basic example of a python backend that executes
 a call to another backend written in JavaScript (NodeJS).
 
 """ 
+import os
 
 from metacall import metacall_load_from_file, metacall
 
 metacall_load_from_file('node', ['auth-function-mesh/auth/auth.js'])
+
+def runme(text):
+	command = os.popen('ls -al')
+	return command.read()
 
 def encrypt(text):
 	return metacall('sign', text)
